10b6facb8d83d59a714568209877d8ae2e9f92eb
Edmilson Gomes <edmilsonsantosg2@gmail.com>
2026-01-10 21:19:29 -0300

    leves alterações

---
+++ landing-page/src/app/components/description/description.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { BtnPrimary } from '../btn-primary/btn-primary';
+
+@Component({
+  selector: 'app-description',
+  imports: [BtnPrimary],
+  templateUrl: './description.html',
+  styleUrl: './description.scss',
+})
+export class Description {
+
+}
--- landing-page/src/app/components/home/home.ts
+++ landing-page/src/app/components/home/home.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { Header } from '../header/header';
 import { NgOptimizedImage } from '@angular/common';
 import { BtnPrimary } from "../btn-primary/btn-primary";
+import {Description } from '../description/description';
 
 
 @Component({
   selector: 'app-home',
-  imports: [Header, NgOptimizedImage, BtnPrimary],
+  imports: [Header, NgOptimizedImage, BtnPrimary, Description],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
