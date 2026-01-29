bae0b8ac3c11a259f6819a5aa77a6648b2a97b1f
Edmilson Gomes <edmilsonsantosg2@gmail.com>
2026-01-29 01:00:58 -0300

    .

--- landing-page/src/app/components/home/home.ts
+++ landing-page/src/app/components/home/home.ts
@@ -1,16 +1,31 @@
 import { Component } from '@angular/core';
 import { Header } from '../header/header';
-import { NgOptimizedImage } from '@angular/common';
+import { NgOptimizedImage, CommonModule } from '@angular/common';
 import { BtnPrimary } from "../btn-primary/btn-primary";
 import {Description } from '../description/description';
 
 
 @Component({
   selector: 'app-home',
-  imports: [Header, NgOptimizedImage, BtnPrimary, Description],
+  standalone: true,
+  imports: [Header, NgOptimizedImage, BtnPrimary, Description, CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
+  heroShoeImage: string = '/assets/NIKE+AIR+MAX+95+BIG+BUBBLE+SE.png';
 
+  dots = [
+    { src: '/assets/NIKE+AIR+MAX+95+BIG+BUBBLE+SE.png', active: true },
+    { src: '/assets/BigBuble2.png', active: false },
+    { src: '/assets/BigBuble3.png', active: false }
+  ];
+
+  changeImage(selectedDot: any) {
+    this.heroShoeImage = selectedDot.src;
+    this.dots.forEach(dot => {
+      dot.active = (dot === selectedDot);
+    });
+  }
 }
+
